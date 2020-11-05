cd04b0873e8c534e279af0a40dff000c9e512fcc
Bhanu Madupati <bhanu.madupati@cision.com>
2020-11-05 12:06:40 -0600

    Initial

---
+++ EmployeeGrid/EmployeeGrid/ColorConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace EmployeeGrid
+{
+    public class ColorConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                DataGridCell cell = (DataGridCell)value;
+                Employee employee = (Employee)cell.DataContext;
+
+                if (employee.Department == "HR")
+                    return Brushes.Blue;
+
+                if (employee.Salary > 60000)
+                    return Brushes.Green;
+
+                if (employee.Salary < 60000)
+                    return Brushes.Red;
+
+                return DependencyProperty.UnsetValue;
+            }
+            catch (Exception e)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ EmployeeGrid/EmployeeGrid/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmployeeGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var employees = new ObservableCollection<Employee>(EmployeeList());
+            var employeeView = new CollectionViewSource { Source = employees }.View;
+            DataContext = new EmployeeViewModel(employeeView);
+        }
+
+        private List<Employee> EmployeeList()
+        {
+            return new List<Employee>()
+            {
+                new Employee
+                {
+                    EmployeeId = 12345,
+                    EmployeeName = "Derek",
+                    Department = "IT",
+                    Salary = 55000.34
+                },
+                 new Employee
+                {
+                    EmployeeId = 383848,
+                    EmployeeName = "Mark",
+                    Department = "HR",
+                    Salary = 61000
+                },
+                 new Employee
+                {
+                    EmployeeId = 2828383,
+                    EmployeeName = "Lisa",
+                    Department = "IT",
+                    Salary = 59999.99
+                },
+                 new Employee
+                {
+                    EmployeeId = 607075,
+                    EmployeeName = "James",
+                    Department = "Operations",
+                    Salary = 80000
+                }
+
+            };
+        }
+    }
+}
